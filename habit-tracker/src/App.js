ce3d394a1b49ece141e17c397296ca209254a278
Kayce Hubbard <ghuleh@Nameless-Ghuleh.local>
2023-01-12 23:46:51 -0600

    Created navbar; made logo

---
+++ habit-tracker/src/App.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react'
+import './App.css';
+
+import Navigation from './components/Navigation'
+
+function App() {
+  return (
+    <Navigation />
+  )
+}
+
+export default App;
